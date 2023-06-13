c5fb1761c3b7cca08fbfa98057d3a223aa7543b4
NUR FARIS PRASTYO <noerfaris@gmail.com>
2023-06-13 10:59:13 +0700

    pengecekan useragent

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,12 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-
-	 public function __construct(){
+	public function __construct(){
 		parent::__construct();
 
 		// Load Model
 		$this->load->model('Mobil_model');
+		
+		// load Library user Agent
+		$this->load->library('user_agent');
 		$this->load->model('Motor_model');
 		$this->load->model('Aksesoris_model');
 
@@ -23,10 +25,10 @@ class Welcome extends CI_Controller {
 
 	public function detailProduct ($url=null)
 	{
-
 		$id = $this->uri->segment('3');
 		$param = $this->uri->segment('4');
 
+		$data['item'] = $this->Mobil_model->getItemByid($id);
 		// var_dump("param", $param);
 		// die;
 
@@ -45,7 +47,14 @@ class Welcome extends CI_Controller {
 
 
 
+	
+		// pengecekan jika website di bukan dari HP atau PC
+		if($this->agent->is_mobile()){
+			echo 'ok';
+
+		}else{
+			$this->load->view('detail', $data);
+		}
 		
-		$this->load->view('detail', $data);
 	}
 }
